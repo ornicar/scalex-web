0287de1f30b99b5cd983684f58ae8ddbee370163
ornicar <thibault.duplessis@gmail.com>
2011-10-10 12:47:32 +0200

    First working version

---
+++ client.js
@@ -0,0 +1,60 @@
+$(function() {
+
+  var $form = $("form.search-form");
+  var $input = $form.find("input");
+  var $results = $(".search-results");
+  var $resultTpl = $("#search-result-template");
+  var xhr;
+
+  if (query = getParameterByName("q")) {
+    $input.val(query);
+    search(query);
+  }
+
+  $input.bind("keyup", function() {
+    if (xhr) xhr.abort();
+    xhr = search($input.val());
+  });
+
+  function search(query) {
+    return $.ajax({
+      url: $form.attr("data-url"),
+      data: { q: query },
+      dataType: "jsonp",
+      success: function(data) {
+        var html = renderResults(data.results);
+        $results.html(html)
+      }
+    });
+  }
+
+  function renderResults(results) {
+    if(results.length == 0) {
+      return "<div class=\"no-results\">Nothing found.</div>";
+    }
+    var html = [], r;
+    for (var i in results) {
+      e = results[i];
+      html += "<div class=\"search-result\">" + $resultTpl.clone()
+        .find(".parent-class").text(e.parent.name).end()
+        .find(".parent-params").text(e.parent.typeParams).end()
+        .find(".name").text(e.name).end()
+        .find(".type-params").text(e.typeParams).end()
+        .find(".params").text(e.valueParams).end()
+        .find(".return").text(e.resultType).end()
+        .html() + "</div>";
+    }
+    return html;
+  }
+
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if(results == null)
+      return "";
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+});
