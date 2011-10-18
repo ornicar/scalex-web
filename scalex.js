374120bb3bcc550a4fac0c9ee7665dd94d2ea298
ornicar <thibault.duplessis@gmail.com>
2011-10-18 15:34:06 +0200

    Setup infinitescroll

--- scalex.js
+++ scalex.js
@@ -20,10 +20,12 @@ $(function() {
 
   // first search: jump to url hash if possible
   if (query = $input.val()) {
-    search(query, function() { 
-      if (window.location.hash && ($fun = $(window.location.hash)).length) {
-        activate($fun);
-        $('html,body').animate({"scrollTop": $fun.offset().top}, 300);
+    search(query, {
+      callback: function() { 
+        if (window.location.hash && ($fun = $(window.location.hash)).length) {
+          activate($fun);
+          $('html,body').animate({"scrollTop": $fun.offset().top}, 300);
+        }
       }
     });
   } else toggle("greetings");
@@ -35,14 +37,18 @@ $(function() {
   $greetings.find('code').each(function() { $(this).wrap('<a href="?q=' + $(this).text() + '">'); });
 
   // toggle between greetings and search results
-  function toggle(s) { $greetings.toggle(s != "search"); $results.toggle(s == "search"); }
+  function toggle(s) { 
+    $(window).unbind("smartscroll");
+    $greetings.toggle(s != "search"); $results.toggle(s == "search"); 
+  }
 
   function activate($fun) {
     $results.find(".active").not($fun).removeClass("active");
     $fun.toggleClass("active");
   }
 
-  function search(query, callback) {
+  function search(query, options) {
+    options = $.extend({page: 1, callback: function() {}, append: false}, options);
     toggle("search");
     xhr = $.ajax({
       url: $form.attr("data-url"),
@@ -52,11 +58,28 @@ $(function() {
         success: function(data) {
           if (data.error) var html = "<div class=\"error\">"+nl2br(data.error)+"</pre>"; 
           else var html = renderResults(data.results); 
-          $results.html(html)
+          if (options.page == 1) html = '<div class="nb_results">' + data.nbResults + ' functions found</div>' + html;
+          if (options.append) $results.append(html);
+          else $results.html(html);
+
           toggle("search");
-          callback && callback();
+          options.callback();
+          if (options.page < data.nbPages) {
+            var end = $results.find(".result:last").offset().top - $(window).height() - 200;
+            $(window).smartscroll(function() {
+              if ($(window).scrollTop() > end) {
+                search(query, { page: options.page + 1, append: true });
+              }
+            });
+          }
         }
     });
+  }
+
+  function changeUrlPage(page) {
+    var url = location.href;
+    if (url.indexOf(/page=/) != -1) return url.replace(/page=\d+/, "page=" + page);
+    else return url + "&page=" + page;
   }
 
   function nl2br(str) {
@@ -64,9 +87,8 @@ $(function() {
   }
 
   function renderResults(results) {
-    if(results.length == 0) { return "<div class=\"no-results\">Nothing found.</div>"; }
-    var html = "";
-    for (var i in results) html += renderResult(results[i]);
+    var html = "", i;
+    for (i in results) html += renderResult(results[i]);
     return html;
   }
 
@@ -122,6 +144,43 @@ $(function() {
     if(results == null) return "";
     else return decodeURIComponent(results[1].replace(/\+/g, " "));
   }
+
+    /* 
+    * smartscroll: debounced scroll event for jQuery *
+    * https://github.com/lukeshumard/smartscroll
+    * Based on smartresize by @louis_remi: https://github.com/lrbabe/jquery.smartresize.js *
+    * Copyright 2011 Louis-Remi & Luke Shumard * Licensed under the MIT license. *
+    */
+
+    var event = $.event,
+		scrollTimeout;
+
+    event.special.smartscroll = {
+        setup: function () {
+            $(this).bind("scroll", event.special.smartscroll.handler);
+        },
+        teardown: function () {
+          console.debug("tear down!");
+            $(this).unbind("scroll", event.special.smartscroll.handler);
+        },
+        handler: function (event, execAsap) {
+            // Save the context
+            var context = this,
+		      args = arguments;
+
+            // set correct event type
+            event.type = "smartscroll";
+
+            if (scrollTimeout) { clearTimeout(scrollTimeout); }
+            scrollTimeout = setTimeout(function () {
+                jQuery.event.handle.apply(context, args);
+            }, execAsap === "execAsap" ? 0 : 100);
+        }
+    };
+
+    $.fn.smartscroll = function (fn) {
+        return fn ? this.bind("smartscroll", fn) : this.trigger("smartscroll", ["execAsap"]);
+    };
 });
 
 //analytics
