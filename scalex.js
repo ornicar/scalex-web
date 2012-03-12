a54dbab6d3cbe18d705c6a935e460d39e4fd1a41
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 23:07:35 +0100

    Remove google analytics tracker

--- scalex.js
+++ scalex.js
@@ -210,18 +210,3 @@ $(function () {
     return fn ? this.bind("smartscroll", fn) : this.trigger("smartscroll", ["execAsap"]);
   };
 });
-
-//analytics
-if (scalexProd) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7935029-5']);
-  _gaq.push(['_trackPageview']);
-  (function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'http://www.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
-}
